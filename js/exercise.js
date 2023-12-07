bacd327e688270c2aa6d7b97af8cc9a664558bbb
Saidaziz007 <saidaminovvs@gmail.com>
2023-12-08 00:22:46 +0500

    end of the task

---
+++ js/exercise.js
@@ -0,0 +1,267 @@
+// Begin1
+
+// let a = +prompt('Kvadrat tomoni: ')
+// let b = +prompt('Kvadrat tomoni: ')
+
+// let P = 2 * (a + b);
+// let S = a * b;
+
+// console.log("Perimetr", P);
+// console.log("Yuzasi", S);
+
+// Begin2
+
+// let d = +prompt('Diametrni kiriting: ')
+
+// const pi = 3.14;
+
+// let L = pi * d;
+// let r = d / 2;
+// let S = pi * r ** 2;
+
+// console.log("Uzunligi", L);
+// console.log("Yuzasi", S);
+
+// Begin3
+
+// let a = +prompt('Yon tomoni: ')
+
+// let V = a ** 3;
+// let S = 6 * a ** 2;
+
+// console.log("Hajimi", V);
+// console.log("To'la sirti", S);
+
+// Begin4
+
+// let a = +prompt('Tomoni-1: ')
+// let b = +prompt('Tomoni-2: ')
+// let c = +prompt('Tomoni-3: ')
+
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+
+// console.log("Hajimi", V);
+// console.log("To'la sirti", S);
+
+// Begin5
+
+// let a = +prompt('Son-1: ')
+// let b = +prompt('Son-2: ')
+
+// let M = (a + b) / 2;
+
+// console.log("O'rta arifmetigi", M);
+
+// Begin6
+
+// let a = +prompt('Katet-1: ');
+// let b = +prompt('Katet-2: ');
+
+// let c = (a ** 2 + b ** 2) ** (1 / 2);
+// let P = a + b + c;
+
+// console.log("Gipotenuzasi", c);
+// console.log("Perimetr", P);
+
+// Begin7
+
+// let r1 = +prompt("Radius-1");
+// let r2 = +prompt("Radius-2");
+
+// const pi = 3.14;
+
+// let S1 = pi * r1;
+// let S2 = pi * r2;
+// let S = pi * (r1 ** 2 - r2 ** 2);
+
+// console.log("Yuza-1", S1);
+// console.log("Yuza-2", S2);
+// console.log("Ayirmasi", S);
+
+// Begin8
+
+// let L = +prompt("Uzunligi: ");
+
+// const pi = 3.14;
+
+// let R = L / (2 * pi);
+// let S = pi * R ** 2;
+
+// console.log("Radius", R);
+// console.log("Yuzasi", S);
+
+// Begin9
+
+// let x1 =+prompt('Nuqta-1');
+// let x2 =+prompt('Nuqta-2');
+// let y1 = +prompt("Nuqta-3");
+// let y2 = +prompt("Nuqta-4");
+
+// let L = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2);
+
+// console.log("Masofa", L);
+
+// Begin10
+
+// let A = +prompt('A: ');
+// let B = +prompt('B: ');
+// let C = +prompt('C: ');
+
+// let k = A;
+
+// A = B;
+// B = C;
+// C = k;
+
+// console.log(A);
+// console.log(B);
+// console.log(C);
+
+// Begin11
+
+// let x = +prompt("X: ");
+
+// let y = (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2;
+
+// console.log(y);
+
+// Begin12
+
+// let A = +prompt('A: ')
+
+// let k = A ** 3;
+// let p = A ** 6;
+// let c = A ** 9;
+// let t = A ** 15;
+
+// console.log(k);
+// console.log(p);
+// console.log(c);
+// console.log(t);
+
+// Begin13
+
+// let Tf = +prompt('Farangeyt: ')
+
+// let Tc = (Tf - 32) * 5 / 9;
+
+// console.log(Tc);
+
+// Begin14
+
+// let X = +prompt('Shokolad: ')
+// let A = +prompt('Shokoladning narxi: ')
+// let Y = +prompt('Konfet: ')
+// let B = +prompt('Konfetning narxi: ')
+
+// let K = A / X - B / Y;
+
+// console.log(K);
+
+// Begin15
+
+// let X = +prompt("Olma: ");
+// let A = +prompt("Olmaning narxi: ");
+// let Y = +prompt("Qizil olma: ");
+// let B = +prompt("Qizil olmaning narxi: ");
+
+// let K = A * X + B * Y;
+
+// console.log(K);
+
+// Integer1
+
+// let abc = +prompt("Uch xonali son: ");
+
+// let res = abc % 10;
+// let res1 = ((abc % 100) - res) / 10;
+
+// console.log(res);
+// console.log(res1);
+
+// Integer2
+
+// let abc = +prompt("Uch xonali son: ");
+
+// let a = abc % 10;
+// let b = ((abc % 100) - a) / 10;
+// let c = (abc - a - b * 10) / 100;
+
+// let abcSum = a + b + c;
+
+// console.log(abcSum);
+
+// Integer3
+
+// let abc = +prompt('Uch xonali son: ')
+
+// let a = abc % 10;
+// let b = ((abc % 100) - a) / 10;
+// let c = (abc - a - b * 10) / 100;
+
+// let abcSum = a * 100 + b * 10 + c;
+
+// console.log(abcSum);
+
+// Integer4
+
+// let abc = +prompt('Uch xonali son: ')
+
+// let a = abc % 10;
+// let b = ((abc % 100) - a) / 10;
+// let c = (abc - a - b * 10) / 100;
+
+// let abcSum = c * 10 + b * 100 + a;
+
+// console.log(abcSum);
+
+// Integer5
+
+// let abc = +prompt('Uch xonali son: ')
+
+// let abcSum = abc + 10 - 1;
+
+// console.log(abcSum);
+
+// Integer6
+
+// let s = +prompt('Minglik son: ')
+
+// let k = (s % 10000 - s % 1000) / 1000;
+
+// console.log(k);
+
+// Integer7
+
+// let s = +prompt('Sekund: ')
+
+// let minut = (s / 60) - (s / 60) % 1;
+
+// console.log("Kun boshidan," + minut + "minut o'tdi");
+
+// Integer8
+
+// let s = +prompt('Sekund')
+
+// let soat = (s / 3600) - ((s / 3600) % 1);
+
+// console.log("Kun boshidan," + soat + "soat o'tdi");
+
+// Integer9
+
+// let s = +prompt('Sekund: ')
+
+// let minut = (s / 60) - (s / 60) % 1;
+// let sec = s % 60;
+
+// console.log("Kun boshidan " + minut + "minutu " + sec + " sekund o'tdi ");
+
+// Integer10
+
+// let s = +prompt("Sekund: ");
+
+// let soat = (s / 3600) - ((s / 3600) % 1);
+// let sec = s % 3600;
+
+// console.log("Kun boshidan " + soat + "soatu " + sec + " sekund o'tdi ");
